33ea7f24c169cdb313d6cf2d5b4251a3e00d9044
Kalinin Denis <dionisi2008@gmail.com>
2023-05-01 16:34:03 +0700

    test

--- Program.cs
+++ Program.cs
@@ -7,12 +7,11 @@ namespace EaseBase // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             ServerBase Сервер = new ServerBase(new EaseBaseApi("http://10.1.2.2:100", "admin", "12345678", "Base1"), "Site", "Denis", "78987811");
-            // Сервер.СозданиеПользователя(new Пользователь("denis", "1234"));
-            // for (int shag = 0; shag <= 1024; shag++)
-            // {
-            //     Сервер.СозданиеПользователя(new Пользователь(shag.ToString(), "1234"));
-            // }
-            // Console.WriteLine("Hello World!");
+            // Сервер.СозданиеПользователя(new Пользователь("Deniska", "78987811"));
+            // Сервер.СозданиеПользователя(new Пользователь("Denisk", "78987811"));
+            // Сервер.СозданиеПользователя(new Пользователь("Deni", "78987811"));
+            // Сервер.СозданиеПользователя(new Пользователь("Den", "78987811"));
+            // System.Console.WriteLine(Сервер.ПоискПользователяВБазе("Deniska"));
             
         }
     }
