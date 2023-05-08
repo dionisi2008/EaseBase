eebbf938dde848b95ec8a947b0740a6024227fe4
Kalinin Denis <dionisi2008@gmail.com>
2023-05-08 15:45:58 +0700

    55

--- Program.cs
+++ Program.cs
@@ -6,14 +6,8 @@ namespace EaseBase // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            ServerBase Сервер = new ServerBase(new EaseBaseApi("http://127.0.0.1:9090", "admin", "12345678", "Base1"), "Site", "Denis", "78987811");
-            
-            
-            Сервер.СозданиеПользователя(new Пользователь("Deniska", "78987811"));
-            Сервер.СозданиеПользователя(new Пользователь("Denisk", "78987811"));
-            Сервер.СозданиеПользователя(new Пользователь("Deni", "78987811"));
-            Сервер.СозданиеПользователя(new Пользователь("Den", "78987811"));
-            System.Console.WriteLine(Сервер.ПоискПользователяВБазе("Deniska"));
+            ServerBase Сервер = new ServerBase(new EaseBaseApi("http://10.1.2.2:100", "admin", "12345678", "Base1"), "Site", "Denis", "78987811");
+            ВебАпи АпиСервераБазыДанных = new ВебАпи(Сервер);
 
         }
     }
---
+++ WebApi.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace EaseBase
+{
+    public class ВебАпи
+    {
+        HttpListener СероверАпиДляЗапросов = new HttpListener();
+        ServerBase БазаДанных;
+        public ВебАпи(ServerBase ДоступКБазеДанных)
+        {
+            
+        }
+    }
+}
