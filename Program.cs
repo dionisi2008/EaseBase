7b3e4bb5f5d2353e3f1860ba0eec79d72bc624f9
Kalinin Denis <dionisi2008@gmail.com>
2023-05-09 17:16:36 +0700

    123

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EaseBase // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            ServerBase Сервер = new ServerBase(new EaseBaseApi("http://10.1.2.2:100", "admin", "12345678", "Base1"), "Site", "Denis", "78987811");
+            ServerBase Сервер = new ServerBase(new EaseBaseApi("http://kalinin-da.ru:100", "admin", "12345678", "Base1"), "Site", "Denis", "78987811");
             ВебАпи АпиСервераБазыДанных = new ВебАпи(Сервер);
 
         }
